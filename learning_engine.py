c2d1f99c25f9e06125ab4408991fb9d97ef33b29
ElabedElmouldi <elabed.elmouldi@gmail.com>
2026-04-15 05:55:04 +0100

    Add weights dictionary and learn function

--- learning_engine.py
+++ learning_engine.py
@@ -1,1 +1,12 @@
+weights = {
+    "trend": 1.0,
+    "volume": 1.0,
+    "momentum": 1.0
+}
 
+def learn(result):
+
+    if result == "WIN":
+        weights["momentum"] += 0.01
+    else:
+        weights["volume"] -= 0.01
