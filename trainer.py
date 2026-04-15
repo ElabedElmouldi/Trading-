d20b63abb2877c6ef11c6efc7ee028fd0fd2ed89
ElabedElmouldi <elabed.elmouldi@gmail.com>
2026-04-15 06:36:29 +0100

    Implement training function for trading agent

--- trainer.py
+++ trainer.py
@@ -1,1 +1,26 @@
 
+from environment import TradingEnv
+from agent import Agent
+from policy import update_q
+from reward import get_reward
+from scanner import get_state
+
+def train():
+
+    env = TradingEnv()
+    agent = Agent()
+
+    for episode in range(500):
+
+        state = get_state()
+        price = 100
+
+        action = agent.act(state)
+
+        reward = env.step(action, price)
+
+        next_state = get_state()
+
+        update_q(agent.q_table, str(state), action, reward)
+
+    return agent
