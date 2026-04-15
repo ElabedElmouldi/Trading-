bbae2fec749b9eb446e7d176440f0235a49c52d0
ElabedElmouldi <elabed.elmouldi@gmail.com>
2026-04-15 06:01:05 +0100

    Update data_feed.py

--- data_feed.py
+++ data_feed.py
@@ -1,1 +1,11 @@
+import ccxt
 
+exchange = ccxt.binance()
+
+def get_symbols():
+    markets = exchange.load_markets()
+    return [s for s in markets if "/USDT" in s][:80]
+
+
+def get_price(symbol):
+    return exchange.fetch_ticker(symbol)["last"]
